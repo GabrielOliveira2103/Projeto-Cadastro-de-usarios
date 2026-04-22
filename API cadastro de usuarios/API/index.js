b389a4b0c3ad2ea2fbd963060c2624a1b8fb113b
gabrieldeoliveira2103 <bieloliveira_51@hotmail.com>
2026-04-22 16:39:02 -0300

    primeiro-commit

---
+++ API cadastro de usuarios/API/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+//mongodb+srv://bieloliveira9531_db_user:FLBwcxxgpZBp4Pml@cadastro.eotrdxo.mongodb.net/Usuarios?appName=cadastro
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb+srv://bieloliveira9531_db_user:FLBwcxxgpZBp4Pml@cadastro.eotrdxo.mongodb.net/Usuarios?appName=cadastro')
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+
+const usuarioSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true },
+},{timestamps: true}
+);
+const Usuario = mongoose.model('Usuario', usuarioSchema);
+
+
+
+
+app.get("/usuarios", async (req, res) => {
+    const usuariosdobanco = await Usuario.find()
+    res.json(usuariosdobanco)
+})
+
+
+app.post("/usuarios", async (req, res) => {
+    console.log(req.body)
+     const usuariocriado = await Usuario.create(req.body)
+
+
+    res.json(usuariocriado)
+
+})
+
+
+app.listen(3003, () => {
+    console.log('Servidor rodando na porta 3003');
+})
---
+++ cadastro de usuarios/cadastro-de-usuarios/src/App.jsx
@@ -0,0 +1,107 @@
+
+import './App.css'
+import { useState, useEffect } from 'react'
+import UserCard from './components/userCard.jsx'
+import axios from 'axios'
+
+function App() {
+
+  const [name, setname] = useState('Gabriel')
+  const [email, setemail] = useState('gabriel@example.com')
+  const [age, setage] = useState(25)
+  const [users, setusers] = useState([])
+
+  async function buscarUsuarios() {
+
+    const resposta = await axios.get('http://localhost:3003/usuarios')
+
+    setusers(resposta.data)
+
+  }
+  useEffect(() => {
+
+    buscarUsuarios()
+
+
+  }
+
+
+
+    , [])
+
+
+
+
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+
+    await axios.post('http://localhost:3003/usuarios', {
+      name: name,
+      email,
+      age: Number(age)
+    })
+
+    await buscarUsuarios()
+
+  }
+
+
+
+  return (
+    <>
+
+
+
+
+      <div className='app'>
+
+        <h1>Cadastro De Usuários</h1>
+
+
+        <form onSubmit={handleSubmit} >
+
+          <input placeholder='Nome'
+            type='text'
+            value={name}
+            onChange={event => setname(event.target.value)}
+
+
+          ></input>
+
+          <input placeholder='Email'
+            type='email'
+            value={email}
+            onChange={event => setemail(event.target.value)}
+
+
+          ></input>
+
+          <input placeholder='Idade'
+            type='number'
+            value={age}
+            onChange={event => setage(event.target.value)}
+
+
+          ></input>
+
+          <button type='submit'>Cadastrar</button>
+
+        </form>
+
+        <div className='user-list'>
+
+          {users.map((user) => (
+            <UserCard key={user._id} user={user} />
+          ))}
+
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
